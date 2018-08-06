28d400278aa0fa66ec7573774cc5a9bb7c83d570
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-06 21:25:35 +0100

    Add find info and rental details by name

--- library.rb
+++ library.rb
@@ -4,4 +4,17 @@ class Library
   def initialize(books)
     @books = books
   end
+
+  def find_info_by_title(name)
+    for book in @books
+      return book if book[:title] == name
+    end
+  end
+
+  def find_rental_details_by_name(name)
+    for book in @books
+      return book[:rental_details] if book[:title] == name
+    end
+  end
+
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -4,17 +4,41 @@ require_relative("../library.rb")
 
 class LibraryTest < MiniTest::Test
   def setup
-    @library_01 = [{
+    @library_01 = [
+    {
     title: "lord_of_the_rings",
     rental_details: {
       student_name: "Jeff",
       date: "01/12/16"
-      }
-    }]
+    }
+    },
+    {
+    title: "foundation",
+    rental_details: {
+      student_name: "Alvaro",
+      date: "03/07/18"
+    }
+    }
+    ]
   end
 
   def test_get_book
     library = Library.new(@library_01)
     assert_equal(@library_01, library.books )
+  end
+
+  def test_find_info_by_title
+    result = @library_01[0]
+    library = Library.new(@library_01)
+    assert_equal(result, library.find_info_by_title("lord_of_the_rings"))
+  end
+
+  def test_find_rental_details_by_name
+    result = {
+      student_name: "Jeff",
+      date: "01/12/16"
+    }
+    library = Library.new(@library_01)
+    assert_equal(result, library.find_rental_details_by_name("lord_of_the_rings"))
   end
 end
